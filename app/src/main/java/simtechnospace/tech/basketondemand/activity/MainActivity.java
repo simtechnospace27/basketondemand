fbd6a0170473aa6a40bc3b7aef13ee8ecb67237e
Mac <simtechnospace27@gmail.com>
2019-06-10 13:18:40 +0530

    Slider Working

--- app/src/main/java/simtechnospace/tech/basketondemand/activity/MainActivity.java
+++ app/src/main/java/simtechnospace/tech/basketondemand/activity/MainActivity.java
@@ -3,9 +3,18 @@ package simtechnospace.tech.basketondemand.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import simtechnospace.tech.basketondemand.R;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 
-public class MainActivity extends AppCompatActivity {
+import simtechnospace.tech.basketondemand.R;
+import simtechnospace.tech.basketondemand.pojoclass.URLClass;
+
+public class MainActivity extends AppCompatActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
+
+    SliderLayout mImageSliderLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +22,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        mImageSliderLayout = findViewById(R.id.slider);
+
+        final String banner_image_url = URLClass.sliderImageUrl;
+
+
+
+        final RequestQueue requestQueue = Volley.newRequestQueue( this );
+        final RequestQueue requestQueueForBannerImages = Volley.newRequestQueue(this);
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
 
     }
 }
---
+++ app/src/main/java/simtechnospace/tech/basketondemand/pojoclass/URLClass.java
@@ -0,0 +1,7 @@
+package simtechnospace.tech.basketondemand.pojoclass;
+
+public class URLClass {
+
+    public static String sliderImageUrl = "http://goviddo.tech/BOD/slider.php";
+
+}
